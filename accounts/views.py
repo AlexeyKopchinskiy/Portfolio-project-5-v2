fd815a5442d080e420840b78bda7a60c9081d004
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-06-29 12:38:19 +0200

    Configure the accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def dashboard_redirect(request):
+    return HttpResponse("Redirecting user based on role...")
