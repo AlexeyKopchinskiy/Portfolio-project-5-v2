6af421c2ef5712f776ed6f315ac3d9dd0ef5d7f0
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-06-29 10:53:19 +0200

    Create 'pages' app with views, URLs, and initial templates for home, about, contact, and pricing pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "pages/home.html")
+
+
+def about(request):
+    return render(request, "pages/about.html")
+
+
+def contact(request):
+    return render(request, "pages/contact.html")
+
+
+def pricing(request):
+    return render(request, "pages/pricing.html")
