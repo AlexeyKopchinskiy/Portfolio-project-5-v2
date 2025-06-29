eb075c9d2c52018d733bdd857db66decf96cc880
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-06-29 10:44:09 +0200

    Create app pages. Remove commented code from settings

--- inkwellblog/settings.py
+++ inkwellblog/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -82,16 +83,6 @@ WSGI_APPLICATION = "inkwellblog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
-# }
 
 DATABASES = {
     "default": dj_database_url.config(
