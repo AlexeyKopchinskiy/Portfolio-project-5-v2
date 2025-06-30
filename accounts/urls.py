8a614f6cb984edf0f16275f1d000ef74a2d37dd5
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-06-30 10:07:15 +0200

    Update user redirections based on thier profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,18 @@ from . import views
 
 
 urlpatterns = [
+    path("login/", views.login_view, name="login"),
     # Placeholder route to avoid ImproperlyConfigured error
     path("", lambda request: HttpResponse("Accounts app is alive!")),
+    # Redirect to dashboard based on user group
     path("", views.dashboard_redirect, name="dashboard"),
+    # redirect to specific dashboards based on user group
+    path("dashboard/reader/", views.dashboard_reader, name="dashboard_reader"),
+    path("dashboard/author/", views.dashboard_author, name="dashboard_author"),
+    path(
+        "dashboard/reviewer/",
+        views.dashboard_reviewer,
+        name="dashboard_reviewer",
+    ),
+    path("dashboard/admin/", views.dashboard_admin, name="dashboard_admin"),
 ]
