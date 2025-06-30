4d2b18b1ec130509fad963718f182bf27701c791
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-06-30 21:43:19 +0200

    Add model to pages app

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Page
+
 
 # Register your models here.
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_published", "updated")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title", "content")
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
