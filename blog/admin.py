7d02942b8906e9ec7116da2258dfc513b035515a
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-06-30 21:39:49 +0200

    Configure blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "is_published", "created")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title", "content")
+    list_filter = ("is_published", "created")
+    ordering = ("-created",)
