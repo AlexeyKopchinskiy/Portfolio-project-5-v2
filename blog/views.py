a576c8cd26da4ae51753d10c86f7572e31f7428c
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-06-30 21:57:37 +0200

    Add form to the blog.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -12,3 +13,23 @@ def post_list(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, is_published=True)
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("my_posts")
+    else:
+        form = PostForm()
+    return render(request, "blog/create_post.html", {"form": form})
+
+
+@login_required
+def my_posts(request):
+    posts = Post.objects.filter(author=request.user)
+    return render(request, "blog/my_posts.html", {"posts": posts})
