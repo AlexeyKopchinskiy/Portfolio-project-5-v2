69518414886efb64ae0d443e242c1171fe6548cc
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-01 08:39:21 +0200

    Add widget to create blog form

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,24 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "featured_image", "is_published"]
+        widgets = {
+            "title": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter post title",
+                }
+            ),
+            "content": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 6,
+                    "placeholder": "Write your post content here...",
+                }
+            ),
+            "featured_image": forms.ClearableFileInput(
+                attrs={"class": "form-control"}
+            ),
+            "is_published": forms.CheckboxInput(
+                attrs={"class": "form-check-input"}
+            ),
+        }
