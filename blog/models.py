9e5b523484e1295ba7ed72a05ae19672cfbf9b24
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-03 16:21:03 +0200

    Add Sommernote to the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django_summernote.fields import SummernoteTextField
 
 
 # Create your models here.
@@ -9,7 +10,7 @@ from django.utils.text import slugify
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
-    content = models.TextField()
+    content = SummernoteTextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = models.ImageField(
         upload_to="blog_images/", blank=True, null=True
