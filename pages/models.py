fdcd8e0f0a12f214ed23e71e6798db6bcda0692b
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-07 10:29:18 +0200

    Add ContactMessage model to store contact data upon contact form submission

--- pages/models.py
+++ pages/models.py
@@ -13,3 +13,14 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=150)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} – {self.subject}"
