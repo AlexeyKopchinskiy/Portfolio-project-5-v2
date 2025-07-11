a6107564e6b1b5d5d5f1a09e7ed508efa33ab773
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-11 14:37:31 +0200

    Register the Models in the admin view

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Newsletter, Subscriber
+
 
 # Register your models here.
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ("subject", "created_at", "scheduled_send", "sent")
+    list_filter = ("sent", "scheduled_send")
+    search_fields = ("subject", "content")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ("email", "name", "subscribed_at", "active")
+    list_filter = ("active",)
+    search_fields = ("email", "name")
+    ordering = ("-subscribed_at",)
