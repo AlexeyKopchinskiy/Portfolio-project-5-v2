86ab93f7a6dc701e8a45cf5231d7ff2caa89f737
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-11 14:35:05 +0200

    Create Newsletter and Subscriber custom models

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Newsletter(models.Model):
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    scheduled_send = models.DateTimeField(blank=True, null=True)
+    sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subject
+
+
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=150, blank=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
