6fd73dd77c2d1cdc59c1fa36c1da4bc42ab4f9c2
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-14 12:55:32 +0200

    Add view to the Newsletter subscription

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SubscriberForm
+from django.contrib import messages
+
 
 # Create your views here.
+
+
+def subscribe(request):
+    if request.method == "POST":
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "You're subscribed! Welcome to Inkwell Updates."
+            )
+            return redirect("newsletter_thank_you")
+    else:
+        form = SubscriberForm()
+    return render(request, "newsletter/subscribe.html", {"form": form})
+
+
+def thank_you(request):
+    return render(request, "newsletter/thank_you.html")
