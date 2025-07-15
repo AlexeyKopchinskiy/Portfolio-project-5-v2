64e3cbcb0769755d81a7e67affb3dc9a14557e66
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-15 12:10:28 +0200

    Add comment model and respective vews

--- blog/views.py
+++ blog/views.py
@@ -13,9 +13,29 @@ def post_list(request):
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug, is_published=True)
-    return render(request, "blog/post_detail.html", {"post": post})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.order_by("-created_at")
+    form = CommentForm()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("post_detail", pk=pk)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+        },
+    )
 
 
 @login_required
