76da51d7f7d992b830bc2e411302869b6f559aa0
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-16 12:46:31 +0200

    Create newsletter editing views, links and forms

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import SubscriberForm
+from .forms import SubscriberForm, NewsletterForm
 from django.contrib import messages
+from .models import Newsletter
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -22,3 +25,50 @@ def subscribe(request):
 
 def thank_you(request):
     return render(request, "newsletter/thank_you.html")
+
+
+@login_required
+def newsletter_dashboard(request):
+    newsletters = Newsletter.objects.order_by("-created_at")[:5]
+    return render(
+        request, "newsletter/dashboard.html", {"newsletters": newsletters}
+    )
+
+
+@login_required
+def create_newsletter(request):
+    if request.method == "POST":
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            newsletter = form.save(commit=False)
+            newsletter.author = request.user
+            newsletter.save()
+            return redirect("newsletter_dashboard")
+    else:
+        form = NewsletterForm()
+    return render(request, "newsletter/create_newsletter.html", {"form": form})
+
+
+@login_required
+def edit_newsletter(request, pk):
+    newsletter = get_object_or_404(Newsletter, pk=pk)
+    if request.method == "POST":
+        form = NewsletterForm(request.POST, instance=newsletter)
+        if form.is_valid():
+            form.save()
+            return redirect("newsletter_dashboard")
+    else:
+        form = NewsletterForm(instance=newsletter)
+    return render(
+        request,
+        "newsletter/edit_newsletter.html",
+        {"form": form, "newsletter": newsletter},
+    )
+
+
+@login_required
+def view_newsletter(request, pk):
+    newsletter = get_object_or_404(Newsletter, pk=pk)
+    return render(
+        request, "newsletter/view_newsletter.html", {"newsletter": newsletter}
+    )
