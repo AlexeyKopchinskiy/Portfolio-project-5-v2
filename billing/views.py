90d02f42913d987881bf92417282719484f22de3
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-07-22 13:39:41 +0200

    Create checkout structure

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import stripe
+from decouple import config
+
 
 # Create your views here.
+stripe.api_key = config("STRIPE_SECRET_KEY")
+
+
+def create_checkout_session(request):
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {"name": "Demo Product"},
+                    "unit_amount": 1200,  # $12.00
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url="http://localhost:8000/billing/success/",
+        cancel_url="http://localhost:8000/billing/cancel/",
+    )
+    return redirect(session.url)
+
+
+def payment_success(request):
+    return render(request, "billing/success.html")
+
+
+def payment_cancel(request):
+    return render(request, "billing/cancel.html")
