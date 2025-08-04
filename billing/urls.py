cf8f3d945ffcead5544b7130716ee739932de365
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-08-04 15:16:51 +0200

    Clean up webhook-related logic

--- billing/urls.py
+++ billing/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views
-# from .views import stripe_webhook
 
 
 urlpatterns = [
     path("checkout/", views.create_checkout_session, name="checkout"),
     path("success/", views.payment_success, name="payment_success"),
     path("cancel/", views.payment_cancel, name="payment_cancel"),
-    # path("webhook/", stripe_webhook, name="stripe-webhook"),
-    # path("billing/webhook/", views.stripe_webhook, name="stripe-webhook"),
 ]
