bfe1fe5974b468e2e6075fa949ac2707715c6279
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-08-29 17:12:35 +0200

    Create two separate forms for Author and reviewer

--- blog/forms.py
+++ blog/forms.py
@@ -43,6 +43,37 @@ class PostForm(forms.ModelForm):
         }
 
 
+# forms.py
+
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content", "featured_image"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": SummernoteWidget(),
+            "featured_image": forms.ClearableFileInput(
+                attrs={"class": "form-control"}
+            ),
+        }
+
+
+class ReviewerForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["review_status", "reviewer_notes", "is_published"]
+        widgets = {
+            "review_status": forms.Select(attrs={"class": "form-control"}),
+            "reviewer_notes": forms.Textarea(
+                attrs={"class": "form-control", "rows": 6}
+            ),
+            "is_published": forms.CheckboxInput(
+                attrs={"class": "form-check-input"}
+            ),
+        }
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
