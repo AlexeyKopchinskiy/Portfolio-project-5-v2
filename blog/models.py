7fb0fb64bbb045cc30a959809ceb857b573c7ba2
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-09-04 12:02:30 +0200

    Add latest posts sidebar to user dashboards

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django_summernote.fields import SummernoteTextField
+from django.urls import reverse
 
 
 # Create your models here.
@@ -42,6 +43,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.slug)])
 
 
 class Comment(models.Model):
