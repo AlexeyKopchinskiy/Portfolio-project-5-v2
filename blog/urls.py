837e2bff0feca33ec72777b28ad73b5b4abbe38d
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-09-09 11:45:32 +0200

    Adopt media settings for developement

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,8 @@ urlpatterns = [
         name="review_user_post",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
