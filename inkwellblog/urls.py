da3b888a6b11c41de7a18d915945e04b6861d53a
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-09-10 10:44:48 +0200

    Fix handler404 in main urls

--- inkwellblog/urls.py
+++ inkwellblog/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
 
 handler404 = "pages.views.custom_404"
+handler500 = "pages.views.custom_500"
 
 urlpatterns = [
     path("", include("pages.urls")),
@@ -41,6 +41,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = "pages.views.custom_404"
-handler500 = "pages.views.custom_500"
